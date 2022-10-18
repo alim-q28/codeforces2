1a87bd3b7c9c47c94724cbc5899d4d0db95bcdec
Syed741 <116105347+Syed741@users.noreply.github.com>
2022-10-18 22:39:21 +0530

    Create BinarySearch.cpp

---
+++ BinarySearch.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BinSearch(int*A,int n,int x)
+{
+	int l=0,h=n,m;
+	while(l<=h)
+	{
+		m=(l+h)/2;
+		if(A[m]==x)
+		return 1;
+		else
+		{
+			if(A[m]>x)
+			{
+				h=m-1;
+			}
+			else if(A[m]<x)
+			{
+				l=m+1;
+			}
+		}
+	}
+	return -1;
+	
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	int A[n];
+	for(int i=0;i<n;i++)
+	cin>>A[i];
+	int x;
+	cin>>x;
+	cout<<BinSearch(A,n,x);
+	
+	return 0;
+}
